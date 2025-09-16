07790c8178c69b209246fe50a3e988f30f68dff3
angrybonghan <new060717gmail.com>
2025-09-16 13:02:00 +0900

    [Feat]플레이어 기본 이동 및 맵 설정

--- 2025 Game-Engine AP/Assets/PlayerController.cs
+++ 2025 Game-Engine AP/Assets/PlayerController.cs
@@ -4,43 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float speed = 5f;
-
     public float jumpPower = 5f;
-
     public float gravity = -9.8f;
 
     private CharacterController controller;
-
     private Vector3 velocity;
-
     public bool isGrounded;
 
-    // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<CharacterController>();   
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         isGrounded = controller.isGrounded;
+
+        // Grounded 상태에서 아래로 계속 떨어지지 않도록 보정
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; // 살짝 눌러주는 값
+        }
+
+        // 이동 입력
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        
-        Vector3 move = new Vector3(x,0,z);
+        Vector3 move = new Vector3(x, 0, z);
         controller.Move(move * speed * Time.deltaTime);
 
+        // 점프 입력
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
-            {
-                velocity.y = jumpPower;
-
-            }
-            velocity.y += gravity * Time.deltaTime;
-            controller.Move(velocity * Time.deltaTime);
+            velocity.y = jumpPower;
         }
+
+        // 중력 적용
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
+
